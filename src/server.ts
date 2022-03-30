deefdc439a4d7602de2dc398581b3657077b972c
Aaron Nguyen <me@aaronnguyen.dev>
2022-03-30 09:13:25 -0400

    Design API endpoints

--- src/server.ts
+++ src/server.ts
@@ -10,3 +10,34 @@ app.get("/", (_req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/conversations:userId", (req, res) => {
+  const data = conversations.filter((conversation) =>
+    conversation.userIds.includes(req.params.userId)
+  );
+  res.json(data);
+});
+
+app.get("/messages/:conversationId", (req, res) => {
+  // Check if conversationId exists
+  if (
+    !conversations.some(
+      (conversation) => conversation.id === req.params.conversationId
+    )
+  ) {
+    return res.json({ message: "404" });
+  }
+
+  const data = messages.filter(
+    (message) => message.conversationId == req.params.conversationId
+  );
+  res.json(data);
+});
+
+app.get("/users/:userId", (req, res) => {
+  const data = users.find((user) => user.id == req.params.userId);
+  res.json(data ? { data: data } : { message: "404" });
+});
+
+app.listen(port, function () {
+  console.log(`[!] Server is running at http://localhost:${port}/`);
+});
